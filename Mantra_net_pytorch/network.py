2169d77f53864239f3deca34745edfa3fb4044d0
bois <next.to.you@qq.com>
2020-11-07 17:41:51 +0800

    完整网络

--- Mantra_net_pytorch/network.py
+++ Mantra_net_pytorch/network.py
@@ -1,4 +1,3 @@
-from typing import Sequence
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -7,91 +6,128 @@ from convlstm import ConvLSTM
 
 
 class SRMConv2D(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, in_channels, out_channels, stride=1, padding=2):
         super(SRMConv2D, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.padding = padding
+        self.SRMWeights = self._get_srm_list()
 
-    def forward(self):
-        pass
+    def _get_srm_list(self):
+        # srm kernel 1
+        srm1 = [[0,  0, 0,  0, 0],
+                [0, -1, 2, -1, 0],
+                [0,  2, -4, 2, 0],
+                [0, -1, 2, -1, 0],
+                [0,  0, 0,  0, 0]]
+        srm1 = torch.tensor(srm1, dtype=torch.float32) / 4.
+
+        # srm kernel 2
+        srm2 = [[-1, 2, -2, 2, -1],
+                [2, -6, 8, -6, 2],
+                [-2, 8, -12, 8, -2],
+                [2, -6, 8, -6, 2],
+                [-1, 2, -2, 2, -1]]
+        srm2 = torch.tensor(srm2, dtype=torch.float32) / 12.
+
+        # srm kernel 3
+        srm3 = [[0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 1, -2, 1, 0],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]]
+        srm3 = torch.tensor(srm3, dtype=torch.float32) / 2.
+
+        return torch.stack([torch.stack([srm1, srm1, srm1], dim=0), torch.stack([srm2, srm2, srm2], dim=0), torch.stack([srm3, srm3, srm3], dim=0)], dim=0)
+
+    def forward(self, X):
+        # X1 =
+        return F.conv2d(X, self.SRMWeights, stride=self.stride, padding=self.padding)
 
 
-class BayarConv2D(nn.Module):
-    def __init__(self) -> None:
-        super(BayarConv2D, self).__init__()
-
-    def forward(self):
-        pass
+# def BayarConstraint(params):
 
 
 class CombinedConv2D(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=3):
         super(CombinedConv2D, self).__init__()
+        self.conv2d = nn.Conv2d(
+            in_channels=in_channels, out_channels=10, stride=1, kernel_size=5, padding=2)
+        self.bayarConv2d = nn.Conv2d(
+            in_channels=in_channels, out_channels=3, stride=1, kernel_size=5, padding=2)
+        self.SRMConv2d = SRMConv2D(
+            in_channels=3, out_channels=3, stride=1, padding=2)
 
-    def forward(self):
-        pass
+    def forward(self, X):
+        X1 = F.relu(self.conv2d(X))
+        X2 = F.relu(self.bayarConv2d(X))
+        X3 = F.relu(self.SRMConv2d(X))
+        return torch.cat([X1, X2, X3], dim=1)
 
 
 class FeatexVGG16(nn.Module):
     def __init__(self, type=1):
-        super(FeatexVGG16).__init__()
+        super(FeatexVGG16, self).__init__()
         # block1
         self.block1 = nn.Sequential(OrderedDict([
-            ('b1c1', CombinedConv2D()),
+            ('b1c1', CombinedConv2D(in_channels=3)),
             ('b1c2', nn.Conv2d(
-                in_channels=32, out_channels=32, stride=1, padding=1)),
-            ('b1ac', nn.ReLU)
+                in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)),
+            ('b1ac', nn.ReLU())
         ]))
 
         # block2
         self.block2 = nn.Sequential(OrderedDict([
             ('b2c1', nn.Conv2d(
-                in_channels=32, out_channels=64, stride=1, padding=1)),
+                in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)),
             ('b2ac1', nn.ReLU()),
             ('b2c2', nn.Conv2d(in_channels=64,
-                               out_channels=64, stride=1, padding=1)),
-            ('b2ac2', nn.ReLU)
+                               out_channels=64, kernel_size=3, stride=1, padding=1)),
+            ('b2ac2', nn.ReLU())
         ]))
 
         # block3
         self.block3 = nn.Sequential(OrderedDict([
             ('b3c1', nn.Conv2d(
-            in_channels=64, out_channels=128, stride=1, padding=1)),
+                in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)),
             ('b3ac1', nn.ReLU()),
-            ('b3c2', nn.Conv2d(in_channels=128, out_channels=128, stride=1, padding=1)),
+            ('b3c2', nn.Conv2d(in_channels=128, kernel_size=3,
+                               out_channels=128, stride=1, padding=1)),
             ('b3ac2', nn.ReLU()),
             ('b3c3', nn.Conv2d(in_channels=128,
-                               out_channels=128, stride=1, padding=1)),
+                               out_channels=128, kernel_size=3, stride=1, padding=1)),
             ('b3ac3', nn.ReLU())
         ]))
 
-
         # block4
         self.block4 = nn.Sequential(OrderedDict([
-                ('b4c1', nn.Conv2d(
-                    in_channels=128, out_channels=256, stride=1, padding=1)),
-                ('b4ac1', nn.ReLU()),
-                ('b4c2', nn.Conv2d(in_channels=256,
-                                   out_channels=256, stride=1, padding=1)),
-                ('b4ac2', nn.ReLU()),
-                ('b4c3', nn.Conv2d(in_channels=256,
-                                   out_channels=256, stride=1, padding=1)),
-                ('b4ac3', nn.ReLU())
+            ('b4c1', nn.Conv2d(
+                in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)),
+            ('b4ac1', nn.ReLU()),
+            ('b4c2', nn.Conv2d(in_channels=256,
+                               out_channels=256, kernel_size=3, stride=1, padding=1)),
+            ('b4ac2', nn.ReLU()),
+            ('b4c3', nn.Conv2d(in_channels=256,
+                               out_channels=256, kernel_size=3, stride=1, padding=1)),
+            ('b4ac3', nn.ReLU())
         ]))
 
         # block5
-        self.block5 = nn.Sequence(OrderedDict([
+        self.block5 = nn.Sequential(OrderedDict([
             ('b5c1', nn.Conv2d(
-                in_channels=256, out_channels=256, stride=1, padding=1))
+                in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)),
             ('b5ac1', nn.ReLU()),
             ('b5c2', nn.Conv2d(in_channels=256,
-                               out_channels=256, stride=1, padding=1)),
+                               out_channels=256, kernel_size=3, stride=1, padding=1)),
             ('b5ac2', nn.ReLU())
         ]))
 
-        self.transform = nn.Conv2d(in_channels=256, out_channels=256, stride=1, padding=1)
-        self.activation = None if type>=1 else nn.Tanh()
+        self.transform = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.activation = None if type >= 1 else nn.Tanh()
 
-
-    def farward(self, X):
+    def forward(self, X):
         X = self.block1(X)
         X = self.block2(X)
         X = self.block3(X)
@@ -101,7 +137,6 @@ class FeatexVGG16(nn.Module):
         if self.activation is not None:
             X = self.activation(X)
         return nn.functional.normalize(X, 2, dim=-1)
-
 
 
 class ZPool2D(nn.Module):
@@ -119,23 +154,26 @@ class ZPool2D(nn.Module):
 
 
 class ZPool2DGlobal(nn.Module):
-    def __init__(self):
+    def __init__(self, size=[1, 64, 1, 1], epsilon=1e-5):
         super(ZPool2DGlobal, self).__init__()
+        self.epsilon = epsilon
+        self.weight = nn.Parameter(torch.zeros(size), requires_grad=True)
 
     def forward(self, X):
         mu = torch.mean(X, dim=(2, 3), keepdim=True)
         D = X - mu
         sigma = torch.sqrt((torch.pow(X, 2) - torch.pow(mu, 2)
-                            ).sum() / (X.shape[-2] * X.shape[-1]))
+                            ).sum(dim=(-1, -2), keepdim=True) / (X.shape[-2] * X.shape[-1]))
+        sigma = torch.max(sigma, self.epsilon + self.weight)
         return D / sigma
 
 
 class MantraNet(nn.Module):
     def __init__(self, Featex=None, pool_size_list=[7, 15, 31]):
         super(MantraNet, self).__init__()
-        # self.rf = Featex
+        self.rf = Featex
         self.outlierTrans = nn.Conv2d(
-            in_channels=3, out_channels=64, kernel_size=(1, 1), bias=False)
+            in_channels=256, out_channels=64, kernel_size=(1, 1), bias=False)
         self.bnorm = nn.BatchNorm2d(num_features=64)
         self.zpoolglobal = ZPool2DGlobal()
         zpools = OrderedDict()
@@ -149,7 +187,8 @@ class MantraNet(nn.Module):
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, X):
-        # X = self.rf(X)
+        if self.rf is not None:
+            X = self.rf(X)
         X = self.bnorm(self.outlierTrans(X))
         Z = []
         Z.append(torch.unsqueeze(self.zpoolglobal(X), dim=1))
@@ -162,27 +201,7 @@ class MantraNet(nn.Module):
         return output
 
 
-if __name__ == "__main__":
-    X = torch.randn([1, 3, 256, 256])
-    # zpool2d = ZPool2DGlobal()
-    # zpool2d1 = ZPool2D(kernel_size=7)
-    # zpool2d2 = ZPool2D(kernel_size=15)
-    # zpool2d3 = ZPool2D(kernel_size=31)
-    # y = zpool2d(X)
-    # y1 = zpool2d1(X)
-    # y2 = zpool2d2(X)
-    # y3 = zpool2d3(X)
-    # print(y.shape, y1.shape, y2.shape, y3.shape)
-    # y = torch.unsqueeze(y, dim=1)
-    # y3 = torch.unsqueeze(y3, dim=1)
-    # y2 = torch.unsqueeze(y2, dim=1)
-    # y1 = torch.unsqueeze(y1, dim=1)
-    # Y = torch.cat([y, y3, y2, y1], dim=1)
-    # hidden_state = torch.zeros_like(y)
-    # conv2dlstm = ConvLSTM(3, 8, (7, 7), 1, batch_first=True)
-    # last_output, last_state = conv2dlstm(Y)
-    # output = last_output[0][:, -1, :, :, :]
-    # print(output.shape)
-    net = MantraNet()
-    output = net(X)
-    print(output.shape)
+X = torch.randn([1, 3, 256, 256])
+net = MantraNet(FeatexVGG16())
+Y = net(X)
+print(Y.shape)
